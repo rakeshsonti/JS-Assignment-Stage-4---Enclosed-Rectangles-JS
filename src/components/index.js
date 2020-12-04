3a70c841a211bc2aff0707d36e979dc6b042c986
rambhajansonti <ramsonti0597@gmail.com>
2020-12-04 18:07:15 +0530

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,65 +1,82 @@
-// rec = {
-// 	top: '25px',
-// 	left: '96px',
-// 	width: '64px',
-// 	height: '96px',
-//      children: []
-// }
+//rec = {
+//	top: '25px',
+//	left: '96px',
+//	width: '64px',
+//	height: '96px'
+//}
 
-function updateStructure(rec1,rec2){
+function updateStructure(recA,recB){
 	//write your code
-	if(contains(rec1, rec2)){
-		const relativeDim = relative(rec1, rec2);
-		return { ...rec1, children: [relativeDim] };
-	} else if(contains(rec2, rec1)){
-		const relativeDim = relative(rec2, rec1);
-		return { ...rec2, children: [relativeDim] };
-	} else{
-		return { ...rec1 };
+	if(contains(recA,recB))
+	{
+	  const relativeDim=relative(recA,recB);
+	  return {...recA,children:[relativeDim]};
+	}else if(contains(recB,recA))
+	{
+	  const relativeDim=relative(recB,recA);
+	  return {...recB,children:[relativeDim]};
+	}else{
+	  return {...recA};
 	}
-}
-
-function contains(rec1, rec2){
-	let obj1 = getCoordinates(rec1);
-	let obj2 = getCoordinates(rec2);
-
-	if(obj1.x1 <= obj2.x1 && obj1.y1 <= obj2.y2 && obj1.x2 >= obj2.x2 && obj1.y2 >= obj2.y2){
-		return true;
-	}
-	return false;
-}
-
-function relative(rec1, rec2){
-	let obj1 = getCoordinates(rec1);
-	let obj2 = getCoordinates(rec2);
-
-	const res = {
-		children: rec2.children
-	}
-
-	if(rec2.top)
-		res.top = `${obj2.x1 - obj1.x1}px`;
-	if(rec2.left)
-		res.left = `${obj2.y1 - obj1.y1}px`;
-	if(rec2.height)
-		res.height = rec2.height;
-	if(rec2.width)
-		res.width = rec2.width;
-	if(rec2.bottom)
-		res.bottom = `${obj1.x2 - obj2.x2}px`;
-	if(rec2.right)
-		res.right = `${obj1.y2 - obj2.y2}px`;
 	
-	return res;
-}
-const T=0;
-const W=0;
-function getCoordinates(rect){
+  }
+  function normalization(rec){
 	return {
-	x1 = rect.top ? parseInt(rect.top) :(T -((parseInt(rect.height) + parseInt(rect.bottom)))),
-	y1 = rect.left ? parseInt(rect.left) :(W -((parseInt(rect.width) + parseInt(rect.right)))),
-	x2 = rect.bottom ?(T- parseInt(rect.bottom)) : (parseInt(rect.height) + parseInt(rect.top)),
-	y2 = rect.right ?(W- parseInt(rect.right)) : (parseInt(rect.width) + parseInt(rect.left))
-}
+	  x1:rec.top?parseInt(rec.top):(T-(parseInt(rec.bottom)+parseInt(rec.height))),
+	  y1:rec.left?parseInt(rec.left):(W-(parseInt(rec.right)+parseInt(rec.width))),
+	  x2:rec.bottom?(T-(parseInt(rec.bottom))):(parseInt(rec.top)+parseInt(rec.height)),
+	  y2:rec.right?(W-parseInt(rec.right)):(parseInt(rec.left)+parseInt(rec.width))
 	}
-module.exports = updateStructure;
+  }
+  function contains(recA,recB)
+  {
+	const recAn=normalization(recA);
+	const recBn=normalization(recB);
+	if(recAn.x1<=recBn.x1
+	  && recAn.y1<=recBn.y1
+	  && recAn.x2>=recBn.x2
+	  && recAn.y2>=recBn.y2
+	  ){
+		return true;
+	  }
+	  return false;
+  }
+  const T=0;
+  const W=0;
+  function relative(recA,recB)
+  {
+	const recAn=normalization(recA);
+	const recBn=normalization(recB);
+	const res={
+	  children:recB.children
+	}
+	if(recB.top)
+	{
+	  res.top=`${recBn.x1-recAn.x1}px`;
+	}
+	if(recB.left)
+	{
+	  res.left=`${recBn.y1-recAn.y1}px`;
+	}
+	if(recB.height)
+	{
+	  res.height=recB.height;
+	}
+	if(recB.width)
+	{
+	  res.width=recB.width;
+	}
+	if(recB.bottom)
+	{
+	  res.bottom=`${recAn.x2-recBn.x2}px`;
+	}
+	if(recB.right)
+	{
+	  res.right=`${recAn.y2-recBn.y2}px`
+	}
+	return res;
+  }
+  
+  
+  module.exports = updateStructure;
+  
